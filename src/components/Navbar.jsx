556179646ad9039901d061653e18f5f971ccb470
rasmeychan34 <raksmeysu34@gmail.com>
2026-04-03 09:23:03 +0700

    Initial portfolio commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 function Navbar() {
   const [active, setActive] = useState("");
 
-  const links = ["projects","skill", "education", "contact"];
+  const links = ["projects", "skill", "education", "contact"];
 
   return (
-    <nav className="sticky top-0 z-50 backdrop-blur-md bg-black/50 border-b border-gray-800">
-      <div className="max-w-6xl mx-auto px-4 py-1 flex justify-between items-center">
+    <nav className="sticky top-0 z-50 w-full backdrop-blur-md bg-black/50 border-b border-gray-800">
+      <div className="w-full max-w-6xl mx-auto px-4 py-1 flex justify-between items-center">
 
         {/* Logo */}
         <h1 className="text-xl font-bold tracking-wide">
